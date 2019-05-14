35841e64a93247e4dbbbdbc1be1c4306392a4fcb
Umisyo <kusunokisouta@gmail.com>
2019-05-15 00:31:10 +0900

    [Fix]:types

--- NewBlock.py
+++ NewBlock.py
@@ -5,6 +5,6 @@ class NewBlock(Block):
     def __init__(self, lastBlock):
         self.index = int(lastBlock.index) + 1
         self.timestamp = str(datetime.now())
-        self.data = input('please input data for new block.\n') + str(self.index)
+        self.data = list(input('please input data for new block.\n') + str(self.index))
         self.previousHash = lastBlock.hash
         self.hash = self.hashBlock()
--- genesis.py
+++ genesis.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class genesisBlock(Block):
     def __init__(self):
         self.index = 0
-        self.data = input('please input data for genesis block.\n')
+        self.data = list(input('please input data for genesis block.\n'))
         self.timestamp = str(datetime.now())
-        self.previousHash = 0
+        self.previousHash = '0'
         self.hash = self.hashBlock()
